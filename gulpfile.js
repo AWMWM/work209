481f2dee0e6fb6d4b5909509d84d87c174c8e625
AWMWM <179592264@qq.com>
2020-11-07 08:14:52 +0800

    8

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+// const http = require('http');
+const connect = require("gulp-connect");
+// const concat = require("gulp-concat");
+// const uglify = require("gulp-uglify");
+// const babel = require("gulp-babel");
+// const rename = require("gulp-rename");
+// const cleanCss = require("gulp-clean-css");
+
+
+//搭建服务器
+gulp.task("server", done => {
+        connect.server({
+            root: "dist",
+            livereload: true
+        })
+        done();
+    })
+    //拷贝文件夹
+gulp.task("copyfile", done => {
+        gulp.src("**").pipe(gulp.dest("dist")).pipe(connect.reload());
+        done();
+    })
+    //监听
+gulp.task("watch", done => {
+    gulp.watch("**"), gulp.series("copyfile");
+    done();
+})
+
+
+
+gulp.task("default", gulp.series("server", "watch"));
